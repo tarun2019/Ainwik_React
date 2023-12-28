a02a0af04b5a1d4095e4e3cf221d72e8eae1135f
tarun1188 <48508911+tarun2019@users.noreply.github.com>
2023-12-28 14:44:59 +0530

    abc

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import reactLogo from '../../logo.svg';
+import { Button } from 'react-bootstrap';
 
 function Home() {
   const homeStyle = {
@@ -10,6 +11,7 @@ function Home() {
     <div className='home' style={{...homeStyle}}>
       <img className="spinnner" src={reactLogo} alt='React logo' width={'500px'}/>
       <h2 style={{font: '50px bold'}}>DEVELOPMENT in PROGRESS...</h2>
+      <Button type='button'>New Button</Button>
     </div>
   );
 }
